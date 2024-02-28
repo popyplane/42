6dbd90a45ea04f5d4f8d8d5f3adabdaa7b4509b8
Baptiste VIEILHESCAZE <bvieilhe@paul-f5Br1s3.clusters.42paris.fr>
2024-02-28 19:10:45 +0100

    cpp02/ex02 ended

---
+++ cpp_rank4/cpp02/ex02/inc/Fixed.hpp
@@ -0,0 +1,33 @@
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+# include <iostream>
+# include <cmath>
+
+using std::cout;
+using std::endl;
+
+class Fixed
+{
+	private :
+		int					_rawValue;
+		static const int	_fractionalPart = 8;
+
+	public :
+		Fixed( void );
+		~Fixed();
+
+		Fixed( int const value );									// new implementation
+		Fixed( float const value );									// new implementation
+		Fixed( Fixed const & src );
+		Fixed &		operator=( Fixed const & rhs );
+
+		float		toFloat( void ) const;							// new implementation
+		int			toInt( void ) const;							// new implementation
+		int			getRawBits( void ) const;
+		void		setRawBits( int const raw );
+};
+
+std::ostream &	operator<<(std::ostream & o, Fixed const & rhs);	// new implementation
+
+#endif
