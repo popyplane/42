b5172b53c2631f30cd6e698b94c0ae337d3a42e8
popyplane <102146503+popyplane@users.noreply.github.com>
2024-12-13 11:24:28 +0000

    Bureaucrat done

---
+++ cpp_rank5/cpp05/inc/Bureaucrat.hpp
@@ -0,0 +1,51 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+# include <iostream>
+# include <exception>
+# include <string>
+
+using	std::string;
+using	std::cout;
+using	std::endl;
+
+class Bureaucrat {
+	
+	
+	private :
+		const string    _name;
+		int             _grade;
+
+	public :
+		// canon
+		Bureaucrat();
+		Bureaucrat(const string name, int grade);
+		Bureaucrat(const Bureaucrat &src);
+		~Bureaucrat();
+		Bureaucrat  &operator=(const Bureaucrat &src);
+
+		// getters
+		const string    getName() const;
+		int             getGrade() const;
+
+		// grade management
+		void			upGrade();
+		void			downGrade();
+
+		// exceptions
+		class GradeTooHighException : public std::exception
+		{
+			public :
+				virtual const char	*what() const throw();
+		}
+
+		class GradeTooLowException : public std::exception
+		{
+			public :
+				virtual const char	*what() const throw(); 
+		}
+}
+
+std::ostream	&operator<<(std::ostream &out, Bureaucrat const &src);
+
+#endif
---
+++ cpp_rank5/cpp05/src/Bureaucrat.cpp
@@ -0,0 +1,73 @@
+#include "../inc/Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat() : _name("default"), _grade(150)
+{
+}
+
+Bureaucrat::Bureaucrat(const string name, int grade) : _name(name), _grade(grade)
+{
+	if (grade < 1)
+		throw(GradeTooHighException);
+	if (grade > 150)
+		throw(GradeTooLowException);
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &src)
+{
+	*this = src;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+}
+
+Bureaucrat		&Bureaucrat::operator=(const Bureaucrat &src)
+{
+	if (this == &other)
+		return (*this);
+	_grade = other.getGrade();
+	return (this);
+}
+
+const string	Bureaucrat::getName() const
+{
+	return(_name);
+}
+
+int				Bureaucrat::getGrade() const
+{
+	return(_grade);
+}
+
+void			Bureaucrat::upGrade()
+{
+	if (_grade > 1)
+		_grade--;
+	else
+		throw(GradeTooHighException);
+}
+
+void			Bureaucrat::downGrade()
+{
+	if (_grade < 149)
+		_grade++;
+	else
+		throw(GradeTooLowException);
+}
+
+const string	Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return ("Grade is too high");
+}
+
+const string	Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return ("Grade is too low");
+}
+
+std::ostream	&operator<<(std::ostream &out, Bureaucrat const &src)
+{
+	out << src.getName() << " , bureaucrat grade " << src.getGrade() << endl
+}
+
+
