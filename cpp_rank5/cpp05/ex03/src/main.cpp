c1e3a9a89f5dd13562fe17a84efc041745bd4c68
popyplane <102146503+popyplane@users.noreply.github.com>
2025-01-24 16:02:30 +0000

    update

--- cpp_rank5/cpp05/ex03/src/main.cpp
+++ cpp_rank5/cpp05/ex03/src/main.cpp
@@ -7,34 +7,64 @@ int main()
 		Bureaucrat	bureaucrat("Bureaucrat", 1);
 		Intern 		intern;
 		AForm		*form;
+	
+		try
+		{
+			std::cout << "\n---------- EX03 Shrubbery Test ----------\n" << std::endl;	
+			form = intern.makeForm("shrubbery creation", "Home");
+			bureaucrat.signForm(*form);
+			bureaucrat.executeForm(*form);
+			delete form;
+		}
+		catch (const std::exception &e)
+		{
+			delete form;
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			std::cout << "\n---------- EX03 Robotomy Test ----------\n" << std::endl;
+			form = intern.makeForm("robotomy request", "Ricky");
+			bureaucrat.signForm(*form);
+			bureaucrat.executeForm(*form);
+			delete form;
+		}
+		catch (const std::exception &e)
+		{
+			delete form;
+			std::cout << e.what() << std::endl;
+		}
 
-		std::cout << "\n---------- EX03 Shrubbery Test ----------\n" << std::endl;
-		form = intern.makeForm("shrubbery creation", "Home");
-		bureaucrat.signForm(*form);
-		bureaucrat.executeForm(*form);
-		delete form;
-
-		std::cout << "\n---------- EX03 Robotomy Test ----------\n" << std::endl;
-		form = intern.makeForm("robotomy request", "Ricky");
-		bureaucrat.signForm(*form);
-		bureaucrat.executeForm(*form);
-		delete form;
-
-		std::cout << "\n---------- EX03 Presidential Test ----------\n" << std::endl;
-		form = intern.makeForm("presidential pardon", "Ivan");
-		bureaucrat.signForm(*form);
-		bureaucrat.executeForm(*form);
-		delete form;
-
-		std::cout << "\n---------- EX03 Invalid Test ----------\n" << std::endl;
-		form = intern.makeForm("Invalid", "Dummy");
-		bureaucrat.signForm(*form);
-		bureaucrat.executeForm(*form);
-		delete form;
+		try
+		{
+			std::cout << "\n---------- EX03 Presidential Test ----------\n" << std::endl;
+			form = intern.makeForm("presidential pardon", "Ivan");
+			bureaucrat.signForm(*form);
+			bureaucrat.executeForm(*form);
+			delete form;
+		}
+		catch (const std::exception &e)
+		{
+			delete form;
+			std::cout << e.what() << std::endl;
+		}
+		try
+		{
+			std::cout << "\n---------- EX03 Invalid Test ----------\n" << std::endl;
+			form = intern.makeForm("Invalid", "Dummy");
+			bureaucrat.signForm(*form);
+			bureaucrat.executeForm(*form);
+			delete form;
+		}
+		catch (const std::exception &e)
+		{
+			delete form;
+			std::cout << e.what() << std::endl;
+		}
 	}
 	catch (const std::exception &e)
 	{
-		std::cout << e.what() << std::endl;
+		std::cout << "Unhandled exception: " << e.what() << std::endl;
 	}
 	return (0);
 }
