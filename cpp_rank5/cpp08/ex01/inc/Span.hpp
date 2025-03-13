5f9cd2fb49389810f31e9057aef2ad62938bb5cf
popyplane <102146503+popyplane@users.noreply.github.com>
2025-03-13 15:10:30 +0000

    cpp08 ex02 updated

--- cpp_rank5/cpp08/ex01/inc/Span.hpp
+++ cpp_rank5/cpp08/ex01/inc/Span.hpp
@@ -4,11 +4,21 @@
 class Span
 {
 	private :
+		Span();
+
+		static const unsigned int	kMaxSpan;
+		
 		const unsigned int	_size;
+		unsigned int		_minSpan;
+		std::multiset<int>	_data;
+
+		// utils
+		void	evaluateMinSpan(std::multiset<int>::iterator it, int n);
+		void	evaluateSpan(int num1, int num2);
+		int		calculateSpan(int num1, int num2);
 
 	public :
 		// canon
-		Span();
 		Span(unsigned int n);
 		Span(const Span &src);
 		~Span();
@@ -16,8 +26,8 @@ class Span
 
 		// members
 		void	addNumber(int n);
-		int		shortestSpan();
-		int		longestSpan();
+		unsigned int		shortestSpan();
+		unsigned int		longestSpan();
 };
 
 #endif
--- cpp_rank5/cpp08/ex01/src/Span.cpp
+++ cpp_rank5/cpp08/ex01/src/Span.cpp
@@ -1,12 +1,78 @@
 #include "../inc/Span.hpp"
 
-Span::Span() : _size(0)
+const unsigned int  Span::kMaxSpan = std::numeric_limits<unsigned int>::max();
+
+Span::Span()
+	: _size(0), _minSpan(kMaxSpan), _data()
 {}
 
-Span::Span(unsigned int n) : _size(n)
+Span::Span(unsigned int n)
+	: _size(n), _minSpan(kMaxSpan), _data()
 {}
 
 Span::Span(const Span &src)
+	: _size(src._size), _minSpan(src._minSpan), _data(src._data)
+{}
+
+Span::~Span()
+{}
+
+Span    &Span::operator=(Span &src)
 {
-    
+	if (this != &src)
+	{
+		_data = src._data;
+		_minSpan = src._minSpan;
+		_size = src._minSpan;
+	}
+	return (*this);
+}
+
+void    Span::addNumber(int n)
+{
+	if (_data.size() >= _size)
+		throw (std::out_of_range("The capacity is full, unable to add a new number"));
+	
+	std::multiset<int>::iterator    it = _data.insert(n);
+	evaluateMinSpan(it, n);
+}
+
+unsigned int	Span::shortestSpan()
+{
+	if (_data.size <= 2)
+		throw (std::logic_error("Not enough numbers to calculate the shortest span"));
+	return (_minSpan);
+}
+
+unsigned int	Span::longestSpan()
+{
+	if (_data.size() <= 2)
+		throw (std::logic_error("Not enough numbers to calculate the longest span"));
+	return (calculateSpan(*(_data.begin()), *(_data.end() - 1)));
+}
+
+void    evaluateMinSpan(std::multiset<int>::iterator it, int n)
+{
+	if (_minSpan == 0)
+		return;
+	if (it != _data.begin())
+		evaluateSpan(*(it - 1), n);
+	if (it != _data.end() - 1)
+		evaluateSpan(*(it + 1), n);
+}
+
+void    evaluateSpan(int num1, int num2)
+{
+	int span = calculateSpan(num1, num2);
+	
+	if (span < _minSpan)
+		_minSpan = span;
+}
+
+unsigned int    calculateSpan(int num1, int num2)
+{
+	long	diff = static_cast<long>(num1) - static_cast<long>(num2);
+	
+	diff = (diff >= 0) ? diff : -diff;
+	return (static_cast<unsigned int>(diff));
 }
