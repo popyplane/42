02231fa9da52564971cc3a2ef75ddebb549079fb
Baptiste Vieilhescaze <baptistevieilhescaze@MacBook-Air-de-Baptiste.local>
2025-04-29 14:41:52 +0200

    mariadb first setup finished

--- inception/srcs/requirements/mariadb/tools/init-mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/init-mariadb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ ! -d "/var/lib/mysql/mysql"]; then
+if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql_install_db --user=mysql
 	mysqld --user=mysql --bootstrap << EOF
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
