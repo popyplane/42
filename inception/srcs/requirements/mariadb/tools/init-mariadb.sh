1f2921395742819ae58d70c554b19e7affb58eba
Baptiste Vieilhescaze <baptistevieilhescaze@MacBook-Air-de-Baptiste.local>
2025-05-01 16:47:03 +0200

    mariadb fixes

--- inception/srcs/requirements/mariadb/tools/init-mariadb.sh
+++ inception/srcs/requirements/mariadb/tools/init-mariadb.sh
@@ -3,6 +3,7 @@
 if [ ! -d "/var/lib/mysql/mysql" ]; then
 	mysql_install_db --user=mysql
 	mysqld --user=mysql --bootstrap << EOF
+USE mysql;
 CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
 CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';
 GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';
